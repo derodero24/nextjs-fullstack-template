80feeb37ae1a122cd341788c83b899aa56f681c4
derodero24 <k.itsh20524abc@gmail.com>
2023-10-04 23:05:44 +0900

    change font weight to download

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -29,7 +29,7 @@ export default function MyDocument() {
         {/* Google Font */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </Head>
